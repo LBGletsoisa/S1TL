8674c9f7e9c12345778885e94a993b945e9dda62
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-06-01 23:45:43 +0200

    Update S1TL.py

--- S1TL.py
+++ S1TL.py
@@ -6,4 +6,4 @@ import streamlit as st
 st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
 st.subheader("Sensor 1")
 
-st.write("Sensor 1 in the basememt tunnel, close to Grizzly section has detetected temperatures lower than the threshold of 35℃")
+st.write("Sensor 1 in the basememt tunnel, close to Grizzly section has detetected temperatures lower than the threshold of 15℃")
